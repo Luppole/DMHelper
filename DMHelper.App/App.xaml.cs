57fc091def5c764276ed28fbee7a8b7b6cec6b20
itama <itamarperetzofficial@gmail.com>
2025-05-03 15:35:59 +0300

    Implement dependency injection and configure services for database and view models

--- DMHelper.App/App.xaml.cs
+++ DMHelper.App/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DMHelper.App.Data;
+using DMHelper.App.Services;
+using DMHelper.App.ViewModels;
 
 namespace DMHelper.App;
 
@@ -9,5 +14,38 @@ namespace DMHelper.App;
 /// </summary>
 public partial class App : Application
 {
+    private ServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        // Database
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlite("Data Source=DMHelper.db"));
+
+        // Services
+        services.AddSingleton<CampaignService>();
+
+        // ViewModels
+        services.AddTransient<MainWindowViewModel>();
+        services.AddTransient<CampaignViewModel>();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var mainWindow = new MainWindow
+        {
+            DataContext = _serviceProvider.GetRequiredService<MainWindowViewModel>()
+        };
+        mainWindow.Show();
+    }
 }
 
